99425527db90386046a380d264472aa201993568
torataku777-debug <torataku777@gmail.com>
2026-09-26 07:15:09 +0900

    Wire Stripe sandbox payment links

--- store/checkout-config.js
+++ store/checkout-config.js
@@ -1,6 +1,8 @@
 window.TAIBAL_CHECKOUT = {
-  lower: "",
-  higher: "",
-  damageFull: "",
-  tournamentFull: ""
+  mode: "test",
+  enabled: false,
+  lower: "https://buy.stripe.com/test_eVqbIU5vTeQW8V14lF7ss00",
+  higher: "https://buy.stripe.com/test_4gMeV63nL9wC6MT8BV7ss04",
+  damageFull: "https://buy.stripe.com/test_3cI5kw8I55gm5IPbO77ss03",
+  tournamentFull: "https://buy.stripe.com/test_aFa6oAe2p24a3AH4lF7ss02"
 };
